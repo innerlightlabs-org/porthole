9048c6e498ee5ea37aaeba883946645b88b63ef8
innerlightlabs1 <innerlightlabs@gmail.com>
2026-05-20 11:08:15 -0400

    fix(cask): align beta URL to monorepo porthole-v<ver> tag shape

--- Casks/porthole@beta.rb
+++ Casks/porthole@beta.rb
@@ -2,7 +2,7 @@ cask "porthole@beta" do
   version "0.1.0-homebrew-cask-beta"
   sha256 "dcc45d72dc96436b607ffa39ef2613f9ce13e8014bb7e5a24d9c1bcbfa94c58e"
 
-  url "https://github.com/innerlightlabs-org/porthole/releases/download/v#{version}/Porthole-#{version}.zip"
+  url "https://github.com/innerlightlabs-org/porthole/releases/download/porthole-v#{version}/Porthole-#{version}.zip"
   name "Porthole (Beta)"
   desc "Menu bar app that surfaces Syncthing status (pre-release channel)"
   homepage "https://github.com/innerlightlabs-org/porthole"
