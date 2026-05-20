63caccbd8aab46a972d7a86ec673c009baf72a1d
innerlightlabs1 <innerlightlabs@gmail.com>
2026-05-20 11:08:15 -0400

    fix(cask): align URL to monorepo porthole-v<ver> tag shape

--- Casks/porthole.rb
+++ Casks/porthole.rb
@@ -2,7 +2,7 @@ cask "porthole" do
   version "0.1.0"
   sha256 "dcc45d72dc96436b607ffa39ef2613f9ce13e8014bb7e5a24d9c1bcbfa94c58e"
 
-  url "https://github.com/innerlightlabs-org/porthole/releases/download/v#{version}/Porthole-#{version}.zip"
+  url "https://github.com/innerlightlabs-org/porthole/releases/download/porthole-v#{version}/Porthole-#{version}.zip"
   name "Porthole"
   desc "Menu bar app that surfaces Syncthing status"
   homepage "https://github.com/innerlightlabs-org/porthole"
